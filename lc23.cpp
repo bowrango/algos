c437166172f13d435fe9a3a7b467248e30988396
matt@gigabug.org <bowrango@gmail.com>
2025-12-02 19:27:53 -0500

    comments

--- lc23.cpp
+++ lc23.cpp
@@ -14,19 +14,19 @@
  */
 class Solution {
 public:
-    // struct Compare {
-    //     bool operator()(ListNode* a, ListNode* b) {
-    //         return a->val > b->val;
-    //     }
-    // };
+    struct Compare {
+        bool operator() (ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
         if(!a) {return b;}
         if(!b) {return a;}
 
-        ListNode dummy(0);
-        ListNode* curr = &dummy;
+        ListNode dummy(0); // stack
+        ListNode* curr = &dummy; // stack
 
-        while (a && b) {
+        while (a && b) { // reuse heap nodes
             if(a->val <= b->val) {
                 curr->next = a;
                 a = a->next;
@@ -37,12 +37,13 @@ public:
             curr = curr->next;
         }
 
+        // handle last
         if(a) {
             curr->next = a;
         } else if (b) {
             curr->next = b;
         }
-        return dummy.next;
+        return dummy.next; // points to heap node
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         // std::priority_queue<ListNode*, vector<ListNode*>, Compare> q;
@@ -51,8 +52,8 @@ public:
         //         q.push(l);
         //     }
         // }
-        // ListNode start(0);
-        // ListNode* curr = &start;
+        // ListNode start(0); // stack
+        // ListNode* curr = &start; // stack
         // while(!q.empty()) {
         //     ListNode* n = q.top();
         //     q.pop();
@@ -62,18 +63,27 @@ public:
         //     curr->next = n;
         //     curr = curr->next;
         // }
-        // return start.next;
+        // return start.next; // points to heap node
 
         if(lists.empty()) {return nullptr;}
         size_t k = lists.size();
         size_t win = 1;
         // merge in pairs of win size
+        // uses O(1) stack memory
         while(win < k) {
             // even indices store the merged list
-            for(size_t i = 0; i < k - win; i+=2*win) {
+            //for(size_t i = 0; i < k; i+=2*win) {
+            for(size_t i = 0; i < k - win; i+=2*win) { // stack
+                // if(i+win < k) {
+                //     merged.push_back(mergeTwoLists(lists[i], lists[i+win]));
+                // } else {
+                //     merged.push_back(lists[i]);
+                // }
                 lists[i] = mergeTwoLists(lists[i], lists[i+win]);
             }
             win *= 2;
+            // lists = merged;
+            // k = lists.size();
         }
         return lists[0];
     }
